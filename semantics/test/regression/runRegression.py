1ded3cea449e424eecdd65fdc6910fd4f1c3b1c4
Alejandro Jiménez Martí <ajimar2001@gmail.com>
2023-03-16 10:36:21 -0400

    tests: Refactor regression script

--- semantics/test/regression/runRegression.py
+++ semantics/test/regression/runRegression.py
@@ -5,13 +5,11 @@ import re
 
 # Run the regression tests
 def runRegression():
+    os.chdir(os.path.dirname(__file__))
     os.chdir('../../../')
     plans_path = 'plexil/test/TestExec-regression-test/plans/'
     scripts_path = 'plexil/test/TestExec-regression-test/scripts/'
-    semantics_path = 'semantics/src/plexilite.maude'
     parsed_tests_path = 'semantics/test/regression/parsedTests/'
-    commands_compile = []
-    commands_parse = []
 
     # Get the list of plans and scripts to compile
     plans = os.listdir(plans_path)
@@ -19,12 +17,51 @@ def runRegression():
     scripts = os.listdir(scripts_path)
     scripts = [script for script in scripts if script.endswith('.pst')]
 
-    # Generate the commands to compile plans and scripts
+    compile_plans_and_scripts(plans_root=plans_path, scripts_root=scripts_path, plans=plans, scripts=scripts)
+
+    # Get the list of compiled plans and scripts
+    compiled_plans = os.listdir(plans_path)
+    compiled_plans = [plan for plan in compiled_plans if plan.endswith('.plx')]
+    compiled_scripts = os.listdir(scripts_path)
+    compiled_scripts = [script for script in compiled_scripts if script.endswith('.psx')]
+    
+    parse_plans_and_scripts(plans_root=plans_path, scripts_root=scripts_path, compiled_plans=compiled_plans, compiled_scripts=compiled_scripts, parsed_tests_root=parsed_tests_path)
+
+    # Get the list of parsed plans and scripts
+    maude_plans = os.listdir(parsed_tests_path + 'plans/')
+        
+    run_tests_and_compare(maude_plans=maude_plans, parsed_tests_root=parsed_tests_path, plans_root=plans_path, scripts_root=scripts_path, compiled_scripts=compiled_scripts)
+
+
+# Check if the plan has a script or return the empty input script
+def get_script(plan_name, compiled_scripts):
+    for script in compiled_scripts:
+        if script[:-4] == plan_name:
+            return script
+    return 'empty.psx'
+
+def get_script_maude(maude_plan_name):
+    maude_scripts = os.listdir('semantics/test/regression/parsedTests/scripts/')
+    for script in maude_scripts:
+        if script[:-6] == maude_plan_name:
+            return script
+    return 'empty.maude'
+
+def remove_extra_whitespace(*, scripts_root, script):
+    tree = ET.parse(scripts_root + script)
+    root = tree.getroot()
+    xml_str = ET.tostring(root, encoding='unicode')
+    clean_xml = re.sub(">\s+?<","><", xml_str)
+    with open(scripts_root + script, 'w') as file:
+        file.write(clean_xml)
+
+def compile_plans_and_scripts(*, plans_root, scripts_root, plans, scripts):
+    commands_compile = []
     for plan in plans:
-        commands_compile.append('plexilc ' + plans_path + str(plan))
+        commands_compile.append('plexilc ' + plans_root + str(plan))
     
     for script in scripts:
-        commands_compile.append('plexilc ' + scripts_path + str(script))
+        commands_compile.append('plexilc ' + scripts_root + str(script))
 
     # Compile plans and scripts in parallel
     n = 8
@@ -33,29 +70,18 @@ def runRegression():
         for p in procs:
             p.wait()
 
-    # Get the list of compiled plans and scripts
-    compiled_plans = os.listdir(plans_path)
-    compiled_plans = [plan for plan in compiled_plans if plan.endswith('.plx')]
-    compiled_scripts = os.listdir(scripts_path)
-    compiled_scripts = [script for script in compiled_scripts if script.endswith('.psx')]
-    
-    # Generate the commands to parse plans and scripts
+def parse_plans_and_scripts(*, plans_root, scripts_root, compiled_plans, compiled_scripts, parsed_tests_root):
+    commands_parse = []
     for plan in compiled_plans:
-        commands_parse.append('plx2maude ' + plans_path + str(plan) + ' >> ' + parsed_tests_path + 'plans/' + str(plan[:-4] + '.maude'))
+        commands_parse.append('plx2maude ' + plans_root + str(plan) + ' >> ' + parsed_tests_root + 'plans/' + str(plan[:-4] + '.maude'))
     
     for script in compiled_scripts:
-        commands_parse.append('psx2maude ' + scripts_path + str(script) + ' >> ' + parsed_tests_path + 'scripts/' +  str(script[:-4] + '.maude'))
-        # Eliminate white spaces in the scripts
-        tree = ET.parse(scripts_path + script)
-        root = tree.getroot()
-        xml_str = ET.tostring(root, encoding='unicode')
-        clean_xml = re.sub(">\s+?<","><", xml_str)
-        with open(scripts_path + script, 'w') as file:
-            file.write(clean_xml)
+        commands_parse.append('psx2maude ' + scripts_root + str(script) + ' >> ' + parsed_tests_root + 'scripts/' +  str(script[:-4] + '.maude'))
+        remove_extra_whitespace(scripts_root=scripts_root, script=script)
 
     # Remove the old plans and scripts
-    os.system('rm ' + parsed_tests_path + 'plans/' + '*.maude')
-    os.system('rm ' + parsed_tests_path + 'scripts/' + '*.maude')
+    os.system('rm ' + parsed_tests_root + 'plans/' + '*.maude')
+    os.system('rm ' + parsed_tests_root + 'scripts/' + '*.maude')
 
     #Parse plans and scripts in parallel
     n = 8
@@ -64,10 +90,7 @@ def runRegression():
         for p in procs:
             p.wait()
 
-    # Get the list of parsed plans and scripts
-    maude_plans = os.listdir(parsed_tests_path + 'plans/')
-        
-    # Run the maude plans and generate the corresponding run.maude
+def run_tests_and_compare(*, maude_plans, parsed_tests_root, plans_root, scripts_root, compiled_scripts):
     for maude_plan in maude_plans:
         os.system('rm semantics/test/regression/output.maude')
         os.system('rm semantics/test/regression/output.plexil')
@@ -89,25 +112,11 @@ q
 '''
             )
         #Run the maude plan with the corresponding script (they have the same name) and plexiltest    
-        os.system('maude -no-ansi-color -no-wrap -no-banner -no-advise -print-to-stderr ' + 'semantics/test/regression/' + 'run.maude ' + parsed_tests_path + 'plans/' + maude_plan + ' ' + parsed_tests_path + 'plans/' + maude_plan + ' > /dev/null 2> semantics/test/regression/output.maude')
+        os.system('maude -no-ansi-color -no-wrap -no-banner -no-advise -print-to-stderr ' + 'semantics/test/regression/' + 'run.maude ' + parsed_tests_root + 'plans/' + maude_plan + ' ' + parsed_tests_root + 'plans/' + maude_plan + ' > /dev/null 2> semantics/test/regression/output.maude')
         plan_script = get_script(maude_plan[:-6], compiled_scripts)
-        os.system('plexiltest -p '+ plans_path + maude_plan[:-6] + '.plx' + ' -s ' + scripts_path + plan_script + ' -d semantics/benchmark/Debug.AcceptanceTest.cfg' + ' > semantics/test/regression/output.plexil 2>&1')
+        os.system('plexiltest -p '+ plans_root + maude_plan[:-6] + '.plx' + ' -s ' + scripts_root + plan_script + ' -d semantics/benchmark/Debug.AcceptanceTest.cfg' + ' > semantics/test/regression/output.plexil 2>&1')
+        print('\n\n' + '--------------------------------' + maude_plan + '--------------------------------' + '\n')
         os.system('plexilog-diff semantics/test/regression/output.plexil semantics/test/regression/output.maude')
-
-
-# Check if the plan has a script or return the empty input script
-def get_script(plan_name, compiled_scripts):
-    for script in compiled_scripts:
-        if script[:-4] == plan_name:
-            return script
-    return 'empty.psx'
-
-def get_script_maude(maude_plan_name):
-    maude_scripts = os.listdir('semantics/test/regression/parsedTests/scripts/')
-    for script in maude_scripts:
-        if script[:-6] == maude_plan_name:
-            return script
-    return 'empty.maude'
 
 def main():
     runRegression()
